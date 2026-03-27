4e185cb7cd8b1a08da9721b4b147cb7e99afb5e2
Illia Zubrytskyi <zubrik1984@yahoo.com>
2026-03-27 10:05:06 +0100

    icon.tsx refactor

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,8 +1,30 @@
+import { ImageResponse } from 'next/og';
+
+export const size = {
+  width: 32,
+  height: 32,
+};
+
+export const contentType = 'image/png';
+
 export default function Icon() {
-  return (
-    <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="64" height="64" rx="18" fill="#111111" />
-      <path d="M20 45.5L32 18L44 45.5H38L32 32.5L26 45.5H20Z" fill="#F7F4EF" />
-    </svg>
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 24,
+          background: 'black',
+          color: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        A
+      </div>
+    ),
+    size
   );
 }
